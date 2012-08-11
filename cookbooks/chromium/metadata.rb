05222715d65f752b8c03828f8e55e34019c0e9bd
Matt Collinge <matt.collinge@gmail.com>
2012-08-11 22:46:35 +0100

    Import chromium version 0.0.1

---
+++ cookbooks/chromium/metadata.rb
@@ -0,0 +1,5 @@
+maintainer       "Chris Peplin"
+maintainer_email "chris.peplin@rhubarbtech.com"
+license          "Apache 2.0"
+description      "Installs/configures Google Chromium packages"
+version          "0.0.1"
