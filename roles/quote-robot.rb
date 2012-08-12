cdeb2302396d07d4608add41c2f65874e3cdbf0a
Matt Collinge <matt.collinge@gmail.com>
2012-08-12 22:43:14 +0100

    2nd attempt

--- roles/quote-robot.rb
+++ roles/quote-robot.rb
@@ -1,5 +1,5 @@
 name "quote-robot"
 description "The base role for our quote robot"
 run_list "recipe[mongodb::default]", "recipe[nodejs]", "recipe[chromium]"
-env_run_lists "_default" => ["recipe[apache2]"], "staging" => ["recipe[apache2::staging]"]
+env_run_lists "_default" => ["recipe[mongodb::default]", "recipe[nodejs]"]
 #default_attributes "mongodb" => { "client_role" => "mcenterprise-validator" }
