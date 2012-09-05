713e2d7df189bdf91b0a567371538bb43cb7d417
Matt Collinge <matt.collinge@gmail.com>
2012-09-05 20:35:10 +0100

    Working on quote robot

--- roles/quote-robot.rb
+++ roles/quote-robot.rb
@@ -1,5 +1,9 @@
 name "quote-robot"
 description "The base role for our quote robot"
-run_list "recipe[mongodb::default]", "recipe[nodejs]", "recipe[chromium]"
-env_run_lists "_default" => ["recipe[mongodb::default]", "recipe[nodejs]"]
+run_list(
+	 "recipe[apt]",
+	 "recipe[mongodb::10gen_repo]", 
+	 "recipe[nodejs]"
+)
+#env_run_lists "_default" => ["recipe[mongodb::default]", "recipe[nodejs]"]
 #default_attributes "mongodb" => { "client_role" => "mcenterprise-validator" }
